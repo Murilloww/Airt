da4e8ac370ca6b996d343c7f4e3bb55f4b6f5dcf
Murillo_Lopes <100168168+Murilloww@users.noreply.github.com>
2024-12-15 13:12:32 -0300

    bugfix geradorObjecao

--- javascript/geradorObjecao.js
+++ javascript/geradorObjecao.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const confirmarBtn = document.querySelector('#confirmar-objecao');
-
-  if (confirmarBtn) {
-      confirmarBtn.addEventListener('click', () => {
-          const objecaoTexto = document.querySelector('input[type="text"]').value;
-
-          if (objecaoTexto) {
-              localStorage.setItem('objecao', objecaoTexto);
-              window.location.href = '/html/geradorDescricao.html';
-          } else {
-              alert('Por favor, insira uma objeção.');
-          }
-      });
-  } else {
-      console.error("Elemento com id 'confirmar-objecao' não encontrado.");
-  }
-});
+    const confirmarBtn = document.querySelector('#confirmar-objecao');
+  
+    if (confirmarBtn) {
+        confirmarBtn.addEventListener('click', () => {
+            const objecaoTexto = document.querySelector('input[type="text"]').value.trim();   
+            const objecaoFinal = objecaoTexto || "nenhuma objeção";
+            localStorage.setItem('objecao', objecaoFinal);     
+            window.location.href = '/html/geradorDescricao.html';
+        });
+    } else {
+        console.error("Elemento com id 'confirmar-objecao' não encontrado.");
+    }
+  });
+  
