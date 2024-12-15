1741607dc4c40a4c21069c8228be9799aaff6b88
Murillo_Lopes <100168168+Murilloww@users.noreply.github.com>
2024-12-15 12:55:14 -0300

    bugfix geradorTema

--- javascript/geradorTema.js
+++ javascript/geradorTema.js
@@ -1,6 +1,18 @@
-document.querySelector('#confirmar-tema').addEventListener('click', () => {
-    const temaSelecionado = document.getElementById('tema').value;
-    localStorage.setItem('tema', temaSelecionado);
-    window.location.href = '/html/geradorDificuldade.html';
-  });
-  
+const selectElement = document.getElementById('tema');
+const confirmarButton = document.getElementById('confirmar-tema');
+
+selectElement.addEventListener('change', () => {
+    if (selectElement.value) {
+        confirmarButton.disabled = false; 
+    } else {
+        confirmarButton.disabled = true; 
+    }
+});
+
+confirmarButton.addEventListener('click', () => {
+    const temaSelecionado = selectElement.value;
+    if (temaSelecionado) {
+        localStorage.setItem('tema', temaSelecionado);
+        window.location.href = '/html/geradorDificuldade.html';
+    }
+});
